7ae9670d4d0142d1594c2f06b34641eaccf109b5
Dmitrii Ubskii <ubskydm@gmail.com>
2017-08-22 16:00:15 +0300

    Iterating over different shapes

--- map.py
+++ map.py
@@ -11,106 +11,118 @@ from datetime import datetime
 import voronoi
 import perlin
 
+
 def dist(a, b):
     return np.linalg.norm(np.array(a) - b)
 
-size = 4096
-bbox = [(0,0),(0,size),(size,size),(size,0)]
 
-print("Loading Voronoi cells...")
+def getVoronoi(size, bbox):
+    print("Loading Voronoi cells...")
 
-centers, neighbors, vertices, regions = np.load('voronoi32k.npy')
+    centers, neighbors, vertices, regions = np.load('voronoi32k.npy')
 
-vert_neighbors = [[] for i in vertices]
-vert_regions = [[] for i in vertices]
-for r, p in enumerate(regions):
-    for i, v in enumerate(p[:-1]):
-        vert_regions[v].append(r)
-        if p[i-1] not in vert_neighbors[v]:
-            vert_neighbors[v].append(p[i-1])
-        if p[i+1] not in vert_neighbors[v]:
-            vert_neighbors[v].append(p[i+1])
+    vert_neighbors = [[] for i in vertices]
+    vert_regions = [[] for i in vertices]
+    for r, p in enumerate(regions):
+        for i, v in enumerate(p[:-1]):
+            vert_regions[v].append(r)
+            if p[i-1] not in vert_neighbors[v]:
+                vert_neighbors[v].append(p[i-1])
+            if p[i+1] not in vert_neighbors[v]:
+                vert_neighbors[v].append(p[i+1])
 
-patches = [Polygon(vertices[p], True, ec='k') for p in regions]
+    return centers, neighbors, vertices, regions
 
-patchCol = PatchCollection(patches, cmap=plt.get_cmap('terrain'))
 
-print("Calculating landscape...")
+def getBasicShape(size, neighbors, vertices, regions, seed=None):
+    if seed is None:
+        seed = datetime.now().microsecond
 
-seed = datetime.now().microsecond
-print("Landmass shape seed: %d" % seed)
-np.random.seed(seed)
+    print("Calculating landmass shape...")
 
-noise = perlin.Perlin(size, 8) #np.load('noise.npy')
+    print("Landmass shape seed: %d" % seed)
+    np.random.seed(seed)
 
-LAND = 0.25
-WATER = 0.15
-OCEAN = 0
+    noise = perlin.Perlin(size, 8)
 
-cell_type = []
+    LAND = 0.25
+    WATER = 0.15
+    OCEAN = 0
 
-def isLand(c):
-    v = [int(t) for t in c]
+    cell_type = []
 
-    x = 2. * c[0] / size - 1
-    y = 2. * c[1] / size - 1
+    def isLand(c):
+        v = [int(t) for t in c]
 
-    return noise[v[0], v[1]] > 0.316 * (1 + x**2 + y**2)
+        x = 2. * c[0] / size - 1
+        y = 2. * c[1] / size - 1
 
-for r in regions:
-    land = 0
-    thresh = 0.7 * len(r)
-    for v in vertices[r]:
-        if np.any([v == 0, v == size]):
-            land = 0
-            break
-        if dist(v, (1756, 666)) < 25:
-            land = 0
-            break
-        land += 1 if isLand(v) else 0
-    cell_type.append(LAND if land > thresh else WATER)
+        return noise[v[0], v[1]] > 0.316 * (1 + x**2 + y**2)
 
-corner = np.where(np.all(vertices == [0,0], axis=1))[0][0]
+    for r in regions:
+        land = 0
+        thresh = 0.7 * len(r)
+        for v in vertices[r]:
+            if np.any([v == 0, v == size]):
+                land = 0
+                break
+            if dist(v, (1756, 666)) < 25:
+                land = 0
+                break
+            land += 1 if isLand(v) else 0
+        cell_type.append(LAND if land > thresh else WATER)
 
-flood = [corner]
+    corner = np.where(np.all(vertices == [0,0], axis=1))[0][0]
 
-cur_index = 0
+    flood = [corner]
 
-while cur_index < len(flood):
-    current = flood[cur_index]
-    cell_type[current] = OCEAN
-    cur_index += 1
+    cur_index = 0
 
-    for n in neighbors[current]:
-        if cell_type[n] == WATER and n not in flood:
-            flood.append(n)
+    while cur_index < len(flood):
+        current = flood[cur_index]
+        cell_type[current] = OCEAN
+        cur_index += 1
 
-patchCol.set_array(np.array(cell_type))
-patchCol.set_clim(0, 1)
-patchCol.set_antialiased(False)
+        for n in neighbors[current]:
+            if cell_type[n] == WATER and n not in flood:
+                flood.append(n)
 
-fig, ax = plt.subplots()
+    return cell_type
 
-ax.add_collection(patchCol)
 
-ax.set_aspect('equal')
-ax.set_xlim(0, size)
-ax.set_ylim(0, size)
-plt.tight_layout()
-plt.axis('off')
+def main():
+    size = 4096
+    bbox = [(0,0),(0,size),(size,size),(size,0)]
 
-plt.figtext(0.05, 0.05, '%d' % seed)
+    centers, neighbors, vertices, regions = getVoronoi(size, bbox)
 
-plt.savefig('shape.png', format='png', dpi=1200)
-plt.show()
+    patches = [Polygon(vertices[p], True, ec='k') for p in regions]
 
-# for p in regions:
-#     plt.plot(*vertices[p].T, c='k', linewidth=1)
-# 
-# for i, ns in enumerate(neighbors):
-#     plt.scatter(*centers[i].T, c='b')
-#     for j in ns:
-#         if j > i:
-#             plt.plot(*centers[[i,j]].T, c='b')
-# 
-# plt.show()
+    patchCol = PatchCollection(patches, cmap=plt.get_cmap('terrain'))
+
+    patchCol.set_clim(0, 1)
+    patchCol.set_antialiased(False)
+
+    fig, ax = plt.subplots()
+
+    ax.add_collection(patchCol)
+
+    ax.set_aspect('equal')
+    ax.set_xlim(0, size)
+    ax.set_ylim(0, size)
+    plt.tight_layout()
+    plt.axis('off')
+
+    for i in range(100):
+        seed = datetime.now().microsecond
+
+        cell_type = getBasicShape(size, neighbors, vertices, regions, seed)
+
+        patchCol.set_array(np.array(cell_type))
+
+        plt.savefig('shapes/%06d.png' % seed, format='png', dpi=1200)
+        # plt.show()
+
+if __name__ == "__main__":
+    main()
+
